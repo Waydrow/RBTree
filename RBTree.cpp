6707871fca34da79085b04679b5476027e1cf2db
Waydrow <waydrow@163.com>
2017-04-24 23:35:22 +0800

    fix bugs

--- RBTree.cpp
+++ RBTree.cpp
@@ -192,7 +192,33 @@ void RBTree<T>::erase(T key) {
         if (color == BLACK)
             eraseFix(child, parent);
         delete node;
+        return;
     }
+
+    // 只有一个孩子或无孩子的情况
+    // 只有左孩子或右孩子, 则直接将孩子放在 node 位置上
+    if (node->left) {
+        child = node->left;
+    } else {
+        child = node->right;
+    }
+    parent = node->parent;
+    color = node->color;
+    child->parent = parent;
+    // node 有父结点
+    if (parent) {
+        // node 为其父节点的左孩子
+        if (node == parent->left)
+            parent->left = child;
+        else
+            parent->right = child;
+    } else {
+        root = child;
+    }
+    if (color == BLACK) {
+        eraseFix(child, parent);
+    }
+    delete node;
 }
 
 // 递归查找节点
